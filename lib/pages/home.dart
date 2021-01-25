4a3b31c873e367255781e98bb8b18cb18073a40a
Alejandro Giubel <agiubel@gmail.com>
2021-01-25 14:32:27 -0500

    Probando ci cd

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Visuales Copy'),
+        title: Text('Visuales Copy probando ci cd'),
         actions: [
           IconButton(
             icon: Icon(Icons.search_rounded),
