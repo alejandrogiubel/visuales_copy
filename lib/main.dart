93d6e37e1851274f42bfc3745b4f5279bef121ad
Alejandro Giubel <agiubel@gmail.com>
2021-01-25 09:58:28 -0500

    Test workflow

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Visuales Copy',
+      title: 'Visuales Copy prueba',
       home: Home(),
     );
   }
